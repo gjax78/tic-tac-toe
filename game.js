53b46a7f4a38d9d216ccdc3906466eae7b4d6d79
Geena Jackson <gjacks303@gmail.com>
2022-01-12 11:32:39 -0700

    Add game class

---
+++ game.js
@@ -0,0 +1,10 @@
+class Game {
+  constructor() {
+    this.player1 = new Player(id1, token1)
+    this.player2 = new Player(id2, token2)
+    this.combinedBoxesClicked = [];
+    this.whoseTurn = 1;
+    this.whoWon = [];
+    this.tie = false;
+  }
+}
