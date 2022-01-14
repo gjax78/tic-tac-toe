d20a73a69f91e98f699d5be04baf9d9412fae774
Geena Jackson <gjacks303@gmail.com>
2022-01-13 18:57:32 -0700

    Create basic functionality

--- main.js
+++ main.js
@@ -1,23 +1,36 @@
 //have a few global variables that will be updated
 //global var game = new instance of Game class
 //global game that you can continually update
+var game = new Game('one', '✕', 'two', '✻')
 
 /* ########## QUERYSELECTORS ########## */
-// var box = document.querySelector('.box')
 var gameBoard = document.querySelector('.game-board')
+var box = document.querySelector('.box')
 
 /* ########## EVENT LISTENERS ########## */
 gameBoard.addEventListener('click', whichSquare)
 
 /* ########## FUNCTIONS ########## */
 function whichSquare(e) {
-  if (e.target.id == 'box-1') {
-    console.log("hello")
+  if (e.target.classList.contains('box')) {
+    clickBox1(event.target)
   }
-  if (e.target.id == 'box-2') {
-    console.log("goodbye")
+}
+
+function clickBox1(box) {
+  if (game.whoseTurn === 1) {
+    player1Turn(box)
+    game.changeTurns()
+  } else if (game.whoseTurn === 2) {
+    player2Turn(box)
+    game.changeTurns()
   }
-  if (e.target.id == 'box-3') {
-    console.log("banana")
-  }
+}
+
+function player1Turn(box) {
+    box.innerHTML = `${game.player1.token}`;
+}
+
+function player2Turn(box) {
+    box.innerHTML = `${game.player2.token}`;
 }
