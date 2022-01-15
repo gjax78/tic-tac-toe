eab84d0e1fbf1d38a6670f02e8c46fd19e01e3b7
Geena Jackson <gjacks303@gmail.com>
2022-01-15 09:08:57 -0700

    Update the game for who won

--- game.js
+++ game.js
@@ -25,27 +25,35 @@ class Game {
 checkRows(player) {
     if (player.boxesClicked.includes('box-1') && player.boxesClicked.includes('box-2') && player.boxesClicked.includes('box-3')) {
       this.win = true
+      this.whoWon.push(player)
     } else if (player.boxesClicked.includes('box-4') && player.boxesClicked.includes('box-5') && player.boxesClicked.includes('box-6')) {
       this.win = true
+      this.whoWon.push(player)
     } else if (player.boxesClicked.includes('box-7') && player.boxesClicked.includes('box-8') && player.boxesClicked.includes('box-9')) {
       this.win = true
+      this.whoWon.push(player)
     }
   }
 
   checkColumns(player) {
     if (player.boxesClicked.includes('box-1') && player.boxesClicked.includes('box-4') && player.boxesClicked.includes('box-7')) {
       this.win = true
+      this.whoWon.push(player)
     } else if (player.boxesClicked.includes('box-2') && player.boxesClicked.includes('box-5') && player.boxesClicked.includes('box-8')) {
       this.win = true
+      this.whoWon.push(player)
     } else if (player.boxesClicked.includes('box-3') && player.boxesClicked.includes('box-6') && player.boxesClicked.includes('box-9')) {
       this.win = true
+      this.whoWon.push(player)
     }
   }
 
   checkDiagonals(player) {
     if (player.boxesClicked.includes('box-1') && player.boxesClicked.includes('box-5') && player.boxesClicked.includes('box-9')) {
       this.win = true
+      this.whoWon.push(player)
     } else if (player.boxesClicked.includes('box-3') && player.boxesClicked.includes('box-5') && player.boxesClicked.includes('box-7')) {
       this.win = true
+      this.whoWon.push(player)
     }
   }
