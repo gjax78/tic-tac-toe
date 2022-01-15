59f41dbb90f20a103cebb4f8fee9e8c8b8578512
Geena Jackson <gjacks303@gmail.com>
2022-01-15 08:58:27 -0700

    Update which boxes each player clicked

--- game.js
+++ game.js
@@ -2,7 +2,9 @@ class Game {
   constructor(id1, token1, id2, token2) {
     this.player1 = new Player(id1, token1);
     this.player2 = new Player(id2, token2);
+    this.gameBoardData = []; //(which boxes each player clicked combined)
     this.whoseTurn = 1;
+    this.win = false;
     this.whoWon = [];
     this.tie = false;
   }
@@ -13,5 +15,10 @@ class Game {
     } else {
       this.whoseTurn = 1
     }
+  }
+
+  playerBoxesClicked(player, boxClicked) {
+    player.boxesClicked.push(boxClicked)
+    this.gameBoardData.push(boxClicked)
   }
 }
